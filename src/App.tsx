2aa4b3e4a04ae6aee978b1817e852d019d5b9947
Lua Winkelbach <60432464+Kathode-Negative@users.noreply.github.com>
2025-10-04 13:35:18 +0200

    map image and barebones of centering

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import SolarSystemPanel from './components/SolarSystemPanel';
-import WorldMapPanel from './components/WorldMapPanel';
+import SolarSystemPanel from './components/UI/OrbitView/SolarSystemPanel';
+import WorldMapPanel from './components/UI/MapView/WorldMapPanel';
 
 const App = () => {
   return (
     <div style={{ display: 'flex', width: '100vw', height: '100vh', margin: 0, background: '#121212', color: '#eee', fontFamily: 'system-ui, Arial, sans-serif', overflow: 'hidden' }}>
       <div style={{ flex: 1, minWidth: 0, display: 'flex' }}>
         <SolarSystemPanel />
+      </div>
+      <div style={{flex: 1, maxWidth:2,display: 'flex', background: '#aaaaaa'}}>
+
       </div>
       <div style={{ flex: 1, minWidth: 0, display: 'flex' }}>
         <WorldMapPanel />
